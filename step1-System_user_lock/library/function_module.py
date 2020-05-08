78b31421157c8491fcd159470f3495166121e263
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-05-08 16:06:45 +0530

    Testing custom module for system_user_lock

--- step1-System_user_lock/library/function_module.py
+++ step1-System_user_lock/library/function_module.py
@@ -32,13 +32,14 @@ def main():
             user_list = systemRefresh.users_list()
         if module.params['bapi_user_lock']['existing_locked_users'] == True:
             existing_locked_users = systemRefresh.existing_locked_users()
-        if module.params['bapi_user_lock']['locked_users'] == True:
-            locked_users = systemRefresh.user_lock(user_list, exception_list, 'lock')
+        if module.params['bapi_user_lock']['lock_users'] == True:
+            locked_users, errors, excempted_users = systemRefresh.user_lock(user_list, exception_list, 'lock')
 
     data = dict()
-    data['User_List'] = user_list
-    data['Existing_locked_users'] = existing_locked_users
-    data['Users_Locked'] = locked_users
+    data["Entire System User List"] = user_list
+    data["User's who's status is already set to Administer Lock"] = existing_locked_users
+    data["Exception user list provided"] = exception_list
+    data["User's Locked with exception to the users list provided"] = locked_users
 
     module.exit_json(changed=True, meta=data)
 
