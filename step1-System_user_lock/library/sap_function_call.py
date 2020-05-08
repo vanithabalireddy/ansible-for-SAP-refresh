be7bd61ddcec98834860581a687e6a77e78568fc
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-05-08 17:24:39 +0530

    Testing custom module for system_user_lock

--- step1-System_user_lock/library/sap_function_call.py
+++ step1-System_user_lock/library/sap_function_call.py
@@ -3,6 +3,39 @@ from ansible.module_utils.basic import *
 from ansible.module_utils.PreSystemRefresh import PreSystemRefresh
 
 
+def bapi_user_lock(module, systemRefresh, params):
+
+    user_list = None
+    existing_locked_users = None
+    locked_users = None
+
+    exception_list = ['SST_TRNG', 'KRISHNA', 'BJOERN', 'DDIC', 'SMAGENDIRAN', 'GIRIDR', 'MRAM']
+    dbiswas = ['SAP*']
+
+    data = dict()
+
+    if params['bapi_user_lock']['user_list']:
+        user_list = systemRefresh.users_list()
+        data["Entire System User List"] = user_list
+
+    if params['bapi_user_lock']['existing_locked_users']:
+        existing_locked_users = systemRefresh.existing_locked_users()
+        data["User's who's status is already set to Administer Lock"] = existing_locked_users
+
+    if params['bapi_user_lock']['lock_users']['action'] == 'lock':
+        list = [user for user in user_list if user not in existing_locked_users]
+        locked_users, errors, excempted_users = systemRefresh.user_lock(list, exception_list, 'lock')
+        data["Exception user list provided to keep them unlocked"] = exception_list
+        data["User's Locked with exception to the users list provided to kept unlocked"] = locked_users
+
+    if params['bapi_user_lock']['lock_users']['action'] == 'unlock':
+        locked_users, errors, excempted_users = systemRefresh.user_lock(user_list, dbiswas, 'unlock')
+        data["User's who's status was already locked prior to the activity"] = dbiswas
+        data["User's Unlocked with exception to the users who's status was already locked prior to the activity"] = locked_users
+
+    module.exit_json(changed=True, meta=data)
+
+
 def main():
     fields = dict(
         bapi_user_lock=dict(
@@ -22,36 +55,9 @@ def main():
 
     systemRefresh = PreSystemRefresh()
 
-    user_list = None
-    existing_locked_users = None
-    locked_users = None
-
-    exception_list = ['SST_TRNG', 'KRISHNA', 'BJOERN', 'DDIC', 'SMAGENDIRAN', 'GIRIDR', 'MRAM']
-    dbiswas = ['SAP*']
-
-    data = dict()
-
     if module.params['bapi_user_lock']:
-        if module.params['bapi_user_lock']['user_list']:
-            user_list = systemRefresh.users_list()
-            data["Entire System User List"] = user_list
-
-        if module.params['bapi_user_lock']['existing_locked_users']:
-            existing_locked_users = systemRefresh.existing_locked_users()
-
-        if module.params['bapi_user_lock']['lock_users']['action'] == 'lock':
-            list = [user for user in user_list if user not in existing_locked_users]
-            locked_users, errors, excempted_users = systemRefresh.user_lock(list, exception_list, 'lock')
-            data["User's who's status is already set to Administer Lock"] = existing_locked_users
-            data["Exception user list provided to keep them unlocked"] = exception_list
-            data["User's Locked with exception to the users list provided to kept unlocked"] = locked_users
-
-        if module.params['bapi_user_lock']['lock_users']['action'] == 'unlock':
-            locked_users, errors, excempted_users = systemRefresh.user_lock(user_list, dbiswas, 'unlock')
-            data["User's who's status was already locked prior to the activity"] = dbiswas
-            data["User's Unlocked with exception to the users who's status was already locked prior to the activity"] = locked_users
-
-    module.exit_json(changed=True, meta=data)
+        params = module.params['bapi_user_lock']
+        bapi_user_lock(module, systemRefresh, params)
 
 
 if __name__ == "__main__":
