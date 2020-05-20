1983ee74483fd378e0df19166d0237f7cd0b0e6b
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-05-20 21:54:54 +0530

    Testing

---
+++ Post-Refresh/step3-Check-background-jobs/library/sap_function_call.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+from ansible.module_utils.basic import *
+from ansible.module_utils.PreSystemRefresh import PreSystemRefresh
+from ansible.module_utils.PostSystemRefresh import PostSystemRefresh
+
+
+# For setting users to Administer Lock and Unlock
+def bapi_user_lock(module, prefresh, params):
+    data = dict()
+
+    try:
+        if params['fetch'] == 'users':
+            user_list = prefresh.users_list()
+            data["Entire System User List"] = user_list
+
+        if params['fetch'] == 'locked_users':
+            existing_locked_users = prefresh.existing_locked_users()
+            data["User's who's status is already set to Administer Lock"] = existing_locked_users
+
+        module.exit_json(changed=True, meta=data)
+
+    except KeyError:
+        if params['lock_users']['action'] == 'lock':
+            user_list = prefresh.users_list()
+            existing_locked_users = prefresh.existing_locked_users()
+            exception_list = params['lock_users']['exception_list']
+
+            active_users = [user for user in user_list if user not in existing_locked_users]
+
+            locked_users, errors, excempted_users = prefresh.user_lock(active_users, exception_list, 'lock')
+
+            data["Exception user list provided to keep them from locking"] = exception_list
+            data["User's Locked with exception to the Exception user list provided ^^"] = locked_users
+
+            module.exit_json(changed=True, meta=data)
+
+        if params['lock_users']['action'] == 'unlock':
+            user_list = prefresh.users_list()
+            exception_list = params['lock_users']['exception_list']
+
+            locked_users, errors, excempted_users = prefresh.user_lock(user_list, exception_list, 'unlock')
+
+            data["User's who's current status is set to Lock(*including existing users that are locked)"] = exception_list
+            data["User's Unlocked with exception to the users who's status was already locked prior to the activity"] = locked_users
+
+            module.exit_json(changed=True, meta=data)
+
+    except Exception as e:
+        data["Error"] = e
+        module.exit_json(changed=False, meta=data)
+
+
+def suspend_bg_jobs(module, prefresh, params):
+    if params:
+        response = prefresh.suspend_bg_jobs()
+        module.exit_json(changed=True, meta={'stdout': response})
+
+
+def export_printers(module, prefresh, params):
+    if params:
+        report = params['report']
+        variant_name = params['variant_name']
+        response = prefresh.export_printer_devices(report, variant_name)
+        module.exit_json(changed=True, meta={'stdout': response})
+
+
+def user_master_export(module, prefresh, params):
+    if params['pc3_ctc_val']:
+        response = prefresh.pc3_ctc_val()
+        module.exit_json(changed=True, meta={'stdout': response})
+    else:
+        report = params['report']
+        variant_name = params['variant_name']
+        response = prefresh.user_master_export(report, variant_name)
+        module.exit_json(changed=True, meta={'stdout': response})
+
+
+def fetch(module, postrefresh, params):
+    if params == 'bgd_val':
+        response = postrefresh.check_background_jobs()
+        module.exit_json(changed=True, meta={'stdout': response})
+
+
+def main():
+    fields = dict(
+        bapi_user_lock=dict(
+            fetch=dict(choices=['users', 'locked_users'], type='str'),
+            lock_users=dict(action=dict(choices=['lock', 'unlock'], required=True),
+                            exception_list=dict(required=True, type='list'), type='dict'),
+            type='dict'),
+        suspend_bg_jobs=dict(type='bool'),
+        export_printers=dict(report=dict(required=True, type='str'),
+                             variant_name=dict(required=True, type='str'), type='dict'),
+        user_master_export=dict(report=dict(type='str'),
+                                variant_name=dict(type='str'),
+                                pc3_ctc_val=dict(default=True, type='bool'), type='dict'),
+        fetch=dict(choices=['bgd_val'], type='str')
+    )
+
+    module = AnsibleModule(
+        argument_spec=fields,
+        supports_check_mode=False
+    )
+
+    if module.check_mode:
+        module.exit_json({"Mes": "CheckMode is not supported as of now!"})
+
+    prefresh = PreSystemRefresh()
+    postrefresh = PostSystemRefresh()
+
+    if module.params['bapi_user_lock']:
+        params = module.params['bapi_user_lock']
+        bapi_user_lock(module, prefresh, params)
+
+    if module.params['suspend_bg_jobs']:
+        params = module.params['suspend_bg_jobs']
+        suspend_bg_jobs(module, prefresh, params)
+
+    if module.params['export_printers']:
+        params = module.params['export_printers']
+        export_printers(module, prefresh, params)
+
+    if module.params['user_master_export']:
+        params = module.params['user_master_export']
+        user_master_export(module, prefresh, params)
+
+    if module.params['fetch']:
+        params = module.params['fetch']
+        fetch(module, postrefresh, params)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Post-Refresh/step3-Check-background-jobs/module_utils/PostSystemRefresh.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+from pyrfc import Connection
+from configparser import ConfigParser
+import os
+from ansible.module_utils.PreSystemRefresh import PreSystemRefresh
+
+
+class PostSystemRefresh(PreSystemRefresh):
+    # for post refresh step3 and step11.
+    def check_background_jobs(self):
+        try:
+            output = self.conn.call("TH_WPINFO")
+        except Exception as e:
+            return "Error while call Function Module: {}".format(e)
+
+        wp_type = []
+        for type in output['WPLIST']:
+            wp_type.append(type['WP_TYP'])
+
+        if 'BGD' in wp_type:
+            return True
+        else:
+            return False
+
+    # Change in Requirement.
+    def import_sys_tables(self):
+        try:
+            self.conn.call("SXPG_COMMAND_EXECUTE", COMMANDNAME='ZTABIMP')
+            return "Successfully Imported Quality System Tables"
+        except Exception as e:
+            return "Error while exporting system tables: {}".format(e)
+
+    def del_old_bg_jobs(self):
+        report = "RSBTCDEL"
+        variant_name = "ZDELLOG"
+
+        desc = dict(
+            MANDT=self.creds['client'],
+            REPORT=report,
+            VARIANT=variant_name
+        )
+
+        content = [{'SELNAME': 'JOBNAME', 'KIND': 'P', 'LOW': '*'},
+                   {'SELNAME': 'USERNAME', 'KIND': 'P', 'LOW': '*'},
+                   {'SELNAME': 'FRM_DATE', 'KIND': 'P', 'LOW': ''},
+                   {'SELNAME': 'FRM_TIME', 'KIND': 'P', 'LOW': ''},
+                   {'SELNAME': 'TO_DATE', 'KIND': 'P', 'LOW': ''},
+                   {'SELNAME': 'TO_TIME', 'KIND': 'P', 'LOW': ''},
+                   {'SELNAME': 'ENDDATE', 'KIND': 'P', 'LOW': ''},
+                   {'SELNAME': 'ENDTIME', 'KIND': 'P', 'LOW': ''},
+                   {'SELNAME': 'FIN', 'KIND': 'P', 'LOW': 'X'},
+                   {'SELNAME': 'ABORT', 'KIND': 'P', 'LOW': 'X'},
+                   {'SELNAME': 'FORCE', 'KIND': 'P', 'LOW': 'X'}]
+
+        text = [{'MANDT': self.creds['client'], 'LANGU': 'EN', 'REPORT': report, 'VARIANT':variant_name, 'VTEXT': 'Delete background jobs logs'}]
+
+        screen = [{'DYNNR': '1000', 'KIND': 'P'}]
+
+        if self.check_variant(report, variant_name) is False:
+            try:
+                self.create_variant(report, variant_name, desc, content, text, screen)
+            except Exception as e:
+                return "Failed to create variant {}: {}".format(variant_name, e)
+
+        if self.check_variant(report, variant_name) is True:
+            try:
+                self.conn.call("SUBST_START_REPORT_IN_BATCH", IV_JOBNAME=report, IV_REPNAME=report, IV_VARNAME=variant_name)
+                return "Old Background jobs logs are successfully deleted."
+            except Exception as e:
+                return "Failed to delete Old Background job logs: {}".format(e)
+        else:
+            return "Creation of variant {} is failed!".format(variant_name)
+
+    # Deletes outbound queues SMQ1 & SMQ2
+    def del_outbound_queues(self, jobname, report, variant_name): #For SMQ1 and SMQ2
+
+        desc = dict(
+            MANDT=self.creds['client'],
+            REPORT=report,
+            VARIANT=variant_name
+        )
+
+        content = [{'SELNAME': 'TID', 'KIND': 'P', 'LOW': '*'},
+                   {'SELNAME': 'PACKAGE', 'KIND': 'P', 'LOW': '10.000'},
+                   {'SELNAME': 'DISPLAY', 'KIND': 'P', 'LOW': 'X'}]
+
+        text = [{'MANDT': self.creds['client'], 'LANGU': 'EN', 'REPORT': report, 'VARIANT':variant_name, 'VTEXT': 'Delete all outbound Queues'}]
+
+        screen = [{'DYNNR': '1000', 'KIND': 'P'}]
+
+        if self.check_variant(report, variant_name) is False:
+            try:
+                self.create_variant(report, variant_name, desc, content, text, screen)
+            except Exception as e:
+                return "Failed to create variant {}: {}".format(variant_name, e)
+
+        if self.check_variant(report, variant_name) is True:
+            try:
+                self.conn.call("SUBST_START_REPORT_IN_BATCH", IV_JOBNAME=jobname, IV_REPNAME=report, IV_VARNAME=variant_name)
+                return "Deleted all Outbound Queues Successfully!"
+            except Exception as e:
+                return "Failed to delete Outbound Queues: {}".format(e)
+        else:
+            return "Failed to create variant {}".format(variant_name)
+
+    def del_trc_queues_sm58(self):
+        jobname = "DELTE_SM58_QUEUES"
+        report = "RSARFCDL"
+        variant_name= "ZDELSM58"
+
+        desc = dict(
+            MANDT=self.creds['client'],
+            REPORT=report,
+            VARIANT=variant_name
+        )
+
+        content = [{'SELNAME': 'TID', 'KIND': 'P', 'LOW': '*'},
+                   {'SELNAME': 'SET_EXEC', 'KIND': 'P', 'LOW': 'X'}]
+
+        text = [{'MANDT': self.creds['client'], 'LANGU': 'EN', 'REPORT': report, 'VARIANT':variant_name, 'VTEXT': 'Delete all TRFC Queues'}]
+
+        screen = [{'DYNNR': '1000', 'KIND': 'P'}]
+
+        if self.check_variant(report, variant_name) is False:
+            try:
+                self.create_variant(report, variant_name, desc, content, text, screen)
+            except Exception as e:
+                return "Failed to create variant {}: {}".format(variant_name, e)
+
+        if self.check_variant(report, variant_name) is True:
+            try:
+                self.conn.call("SUBST_START_REPORT_IN_BATCH", IV_JOBNAME=jobname, IV_REPNAME=report, IV_VARNAME=variant_name)
+                return "Deleted all TRC Queues SM58"
+            except Exception as e:
+                return "Failed to delete Outbound Queues: {}".format(e)
+        else:
+            return "Failed to create variant {}".format(variant_name)
+
+    # Testing - Phase
+    def clean_ccms_data(self):
+        try:
+            self.conn.call("INST_EXECUTE_REPORT", PROGRAM='CSM_TAB_CLEAN')
+            return "Successfully cleansed CCMS data"
+        except Exception as e:
+            return "Error while cleaning CCMS data: {}".format(e)
+
+    # Testing - Phase
+    def check_spool_consistency(self):
+        try:
+            output = self.conn.call("INST_EXECUTE_REPORT", PROGRAM='RSPO1043')
+            # Needs to wait until report is executed successfully.
+            return output
+        except Exception as e:
+            return "Error while checking_spool_consistency: {}".format(e)
+
+    # Implementation phase
+    def se06_post_copy_transport(self):
+        pass
+
+
+# Steps Completed in Post Refresh
+# 1. Quality System User Lock           = Done
+# 2. Suspend background jobs            = Done
+# 3. Check background process           = Done
+# 4. Import Quality System Tab          = Not Done  #FM not callable  (.ctl file to target sap server.)
+# 5. Delete old background jobs         = Done
+# 6. Delete outbound Queues SMQ1        = Done
+# 7. Delete outbound Queues SMQ2        = Done
+# 8. Delete TRC Queues SM58             = Done
+# 9. Clean CCMS data                    = Testing phase
+# 10. Check Spool Consistency           = Testing phase
+# 11. Set background process to normal= Done
+# 11. SE06 – Post copy transport process            = Not Done >            #SAP GUI
+# 12. RDDNEWPP – RDDIMPDP background job execution  = Not Done >            #SAP GUI
+# 13. User Master Import                            = Not Done >            #variants
+# 14. Import all Printer output devices             = Not Done >            #SAP GUI
+# 15. BDLS – Logical system conversion              = Not Done >            #variants
+# 16. ZSCREEN_LOGIN_INFO - change                   = Not Done >            #variants
+# 17. Quality System User Unlock                    = Testing Phase
+
+
