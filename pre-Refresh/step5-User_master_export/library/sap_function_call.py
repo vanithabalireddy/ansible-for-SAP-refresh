0395b1fca894e855894101c4341ec77bd77bafc3
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-06-06 20:21:47 +0530

    Testing fetch module for user master export()

--- pre-Refresh/step5-User_master_export/library/sap_function_call.py
+++ pre-Refresh/step5-User_master_export/library/sap_function_call.py
@@ -59,6 +59,49 @@ class SAPFunctionCall(PreSystemRefresh):
             self.err = "Failed to Execute command {}".format(params['NAME'])
             module.fail_json(msg=self.err, error=to_native(e), exception=traceback.format_exc())
 
+    def fetch(self, module, params):
+        try:
+            output = self.conn.call("RFC_READ_TABLE", QUERY_TABLE='E070L')  # IF Condition check needs to be implemented
+        except Exception as e:
+            self.err = "Failed while querying E070L Table: {}".format(e)
+            module.fail_json(msg=self.err, error=to_native(e), exception=traceback.format_exc())
+
+        result = dict()
+        trans_val = None
+        for data in output['DATA']:
+            for val in data.values():
+                trans_val = ((val.split()[1][:3] + 'C') + str(int(val.split()[1][4:]) + 1))
+                result["trans_val"] = trans_val
+
+        try:
+            output = self.conn.call("RFC_READ_TABLE", QUERY_TABLE='TMSPCONF',
+                                    FIELDS=[{'FIELDNAME': 'NAME'}, {'FIELDNAME': 'SYSNAME'}, {'FIELDNAME': 'VALUE'}])
+        except Exception as e:
+            self.err = "Failed while fetching TMC CTC Value: {}".format(e)
+            module.fail_json(msg=self.err, error=to_native(e), exception=traceback.format_exc())
+
+        ctc = None
+        for field in output['DATA']:
+            if field['WA'].split()[0] == 'CTC' and field['WA'].split()[1] == self.creds['sid']:
+                ctc = field['WA'].split()[2]
+
+        if ctc is '1':
+            sid_ctc_val = self.creds['sid'] + '.' + self.creds['client']
+            result["sid_ctc_val"] = sid_ctc_val
+        else:
+            sid_ctc_val = self.creds['sid']
+            result["sid_ctc_val"] = sid_ctc_val
+
+        result["client"] = self.creds['client']
+        result["sid_val"] = self.creds['sid']
+
+        if trans_val and ctc is not None:
+            self.data = result
+            module.exit_json(changed=True, meta=self.data)
+        else:
+            self.err = "Failed to fetch {}".format(params['sys_params'])
+            module.fail_json(msg=self.err, error=to_native(), exception=traceback.format_exc())
+
 
 # For setting users to Administer Lock and Unlock
 def bapi_user_lock(module, prefresh, params):
@@ -121,6 +164,7 @@ def user_master_export(module, prefresh, params):
 
 def main():
     fields = dict(
+        fetch=dict(choices=['sys_params'], type='str'),
         BAPI_USER_LOCK=dict(
             fetch=dict(choices=['users', 'locked_users'], type='str'),
             lock_users=dict(action=dict(choices=['lock', 'unlock'], required=True),
@@ -152,6 +196,10 @@ def main():
 
     prefresh = PreSystemRefresh()
     functioncall = SAPFunctionCall()
+
+    if module.params['fetch']:
+        params = module.params['fetch']
+        functioncall.fetch(module, params)
 
     if module.params['BAPI_USER_LOCK']:
         params = module.params['BAPI_USER_LOCK']
