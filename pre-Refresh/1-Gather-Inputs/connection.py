f764cb0fd06780da2e3b2d9dd3636527f0ef3ee3
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-07-10 14:25:23 +0530

    Updated connection.py and added debug tasks

--- pre-Refresh/1-Gather-Inputs/connection.py
+++ pre-Refresh/1-Gather-Inputs/connection.py
@@ -5,30 +5,31 @@ import os
 
 
 def main():
-    self.config = ConfigParser()
+    config = ConfigParser()
     try:
-        self.config.read(os.environ["HOME"] + '/.config/sap_config.ini')
-        self.creds = self.config['SAP']
+        config.read(os.environ["HOME"] + '/.config/sap_config.ini')
+        creds = config['SAP']
 
-        self.conn = Connection(user=self.creds['user'], passwd=self.creds['passwd'], ashost=self.creds['ashost'],
-                               sysnr=self.creds['sysnr'], sid=self.creds['sid'], client=self.creds['client'])
+        conn = Connection(user=creds['user'], passwd=creds['passwd'], ashost=creds['ashost'],
+                               sysnr=creds['sysnr'], sid=creds['sid'], client=creds['client'])
 
-        if self.conn:
+        if conn:
             return True
     except KeyError:
-        self.config.read(os.path.expanduser('~') + '\.config\sap_config.ini')
-        self.creds = self.config['SAP']
+         config.read(os.path.expanduser('~') + '\.config\sap_config.ini')
+         creds = config['SAP']
 
-        self.conn = Connection(user=self.creds['user'], passwd=self.creds['passwd'], ashost=self.creds['ashost'],
-                               sysnr=self.creds['sysnr'], sid=self.creds['sid'], client=self.creds['client'])
+         conn = Connection(user=creds['user'], passwd=creds['passwd'], ashost= creds['ashost'],
+                           sysnr=creds['sysnr'], sid=creds['sid'], client=creds['client'])
 
-        if self.conn:
+         if conn:
             return True
     except Exception:
         return False
 
     return False
 
+
 if __name__ == '__main__':
-    main()
+    print(main())
 
